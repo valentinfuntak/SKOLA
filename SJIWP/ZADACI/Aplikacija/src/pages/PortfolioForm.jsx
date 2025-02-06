106407a7624756a08459b9d9979cb57aabc27025
valentinfuntak <valentinfuntakk@gmail.com>
2025-02-06 20:31:13 +0100

    .

--- SJIWP/ZADACI/Aplikacija/src/pages/PortfolioForm.jsx
+++ SJIWP/ZADACI/Aplikacija/src/pages/PortfolioForm.jsx
@@ -1,5 +1,12 @@
 import { createSignal } from "solid-js";
 import { useAuth } from "../backend/AuthProvider.jsx";
+import html from "../assets/Ikone/Frontend/html.png";
+import css from "../assets/Ikone/Frontend/css-3.png";
+import js from "../assets/Ikone/Frontend/js.png";
+import nodejs from "../assets/Ikone/Backend/470-4704009_nodejs-rust-logo-node-js.png";
+import java from "../assets/Ikone/Mobile/icons8-java-48.png";
+
+
 
 export default function PortfolioForm() {
     const session = useAuth();
@@ -75,11 +82,62 @@ export default function PortfolioForm() {
                         </div>
 
                         {/* Tehnologije */}
-                        <div class="flex flex-col gap-6">
-                            <label class="text-lg font-semibold text-gray-300">
-                                Tehnologije
-                            </label>
+                        <div class="flex flex-col gap-6 items-start">
+                            <label class="text-lg font-semibold text-gray-300">Tehnologije</label>
+
+                            {/* Frontend */}
+                            <div class="flex flex-col items-start gap-4">
+                                <h2 class="text-xl font-semibold">Frontend</h2>
+                                <div class="flex gap-6">
+                                    {/* HTML */}
+                                    <div class="flex flex-col items-center gap-2">
+                                        <h1 class="text-lg">HTML</h1>
+                                        <img class="w-15" src={html} alt="HTML" />
+                                        <input type="checkbox" defaultChecked className="checkbox checkbox-primary" />
+                                    </div>
+                                    {/* CSS */}
+                                    <div class="flex flex-col items-center gap-2">
+                                        <h1 class="text-lg">CSS</h1>
+                                        <img class="w-15" src={css} alt="CSS" />
+                                        <input type="checkbox" defaultChecked className="checkbox checkbox-primary" />
+                                    </div>
+                                    {/* JavaScript */}
+                                    <div class="flex flex-col items-center gap-2">
+                                        <h1 class="text-lg">JavaScript</h1>
+                                        <img class="w-15" src={js} alt="JavaScript" />
+                                        <input type="checkbox" defaultChecked className="checkbox checkbox-primary" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* Backend */}
+                            <div class="flex flex-col items-start gap-4">
+                                <h2 class="text-xl font-semibold">Backend</h2>
+                                <div class="flex gap-6">
+                                    {/* Node.js */}
+                                    <div class="flex flex-col items-center gap-2">
+                                        <h1 class="text-lg">Node.js</h1>
+                                        <img class="w-15" src={nodejs} alt="Node.js" />
+                                        <input type="checkbox" defaultChecked className="checkbox checkbox-primary" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* Ostali jezici */}
+                            <div class="flex flex-col items-start gap-4">
+                                <h2 class="text-xl font-semibold">Ostali jezici</h2>
+                                <div class="flex gap-6">
+                                    {/* Java */}
+                                    <div class="flex flex-col items-center gap-2">
+                                        <h1 class="text-lg">Java</h1>
+                                        <img class="w-15" src={java} alt="Java" />
+                                        <input type="checkbox" defaultChecked className="checkbox checkbox-primary" />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+
+
 
                         {/* Iskustvo */}
                         <div class="flex flex-col gap-2">
