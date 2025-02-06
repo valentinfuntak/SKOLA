e9e74b9b207e861e1a6e39287116358d2f6b3595
valentinfuntak <valentinfuntakk@gmail.com>
2025-02-06 20:47:26 +0100

    .

--- SJIWP/ZADACI/Aplikacija/src/layouts/MainLayout.jsx
+++ SJIWP/ZADACI/Aplikacija/src/layouts/MainLayout.jsx
@@ -25,7 +25,7 @@ export default function MainLayout(props) {
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
             </div>
             <ul
               tabIndex={0}
@@ -37,7 +37,7 @@ export default function MainLayout(props) {
           </div>
         </div>
         <div className="navbar-center">
-          <a className="btn btn-ghost text-2xl">{appName}</a>
+          <a className="btn btn-ghost text-3xl">{appName}</a>
         </div>
         <div className="navbar-end">
 
